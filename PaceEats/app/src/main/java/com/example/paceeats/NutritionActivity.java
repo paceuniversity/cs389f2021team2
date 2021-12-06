910fbae7d79d0b47c4edc788df994dd97fd38be9
Kevin Lutz <kevinlutz429@gmail.com>
2021-12-06 16:27:59 -0500

    added custom calorie tracking functionality and UI changes

--- PaceEats/app/src/main/java/com/example/paceeats/NutritionActivity.java
+++ PaceEats/app/src/main/java/com/example/paceeats/NutritionActivity.java
@@ -7,6 +7,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -21,9 +23,12 @@ import com.google.firebase.database.FirebaseDatabase;
 public class NutritionActivity extends AppCompatActivity {
 
     FirebaseAuth mAuth;
-    TextView calorieGoalString;
+    TextView calorieGoalString, currentCaloriesText;
     DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
     FirebaseUser currentUser;
+    ProgressBar calorieBar;
+
+    EditText customCals, customCO2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,18 +39,28 @@ public class NutritionActivity extends AppCompatActivity {
         currentUser = mAuth.getCurrentUser();
 
         calorieGoalString = findViewById(R.id.calorieGoal);
+        currentCaloriesText = findViewById(R.id.currentCaloriesText);
+
+        calorieBar = findViewById(R.id.calorieProgressBar);
+
+        customCals = findViewById(R.id.inputCustomCals);
+        customCO2 = findViewById(R.id.inputCustomCO2);
 
         mDatabase.child("Users").child(currentUser.getUid()).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
             @Override
             public void onComplete(@NonNull Task<DataSnapshot> task) {
                 if (!task.isSuccessful()) {
                     Log.e("firebase", "Error getting data", task.getException());
+
                 }
                 else {
                     Log.d("firebase", String.valueOf(task.getResult().getValue()));
                     Log.d("firebase", task.getResult().child("calorieGoal").getValue().toString());
                     String calorieGoal = task.getResult().child("calorieGoal").getValue().toString();
+                    String currentCalories = task.getResult().child("currentCalories").getValue().toString() + " of";
                     calorieGoalString.setText(calorieGoal);
+                    currentCaloriesText.setText(currentCalories);
+                    calorieBar.setProgress(Integer.parseInt(calorieGoal));
                 }
             }
         });
@@ -73,5 +88,38 @@ public class NutritionActivity extends AppCompatActivity {
     public void submitFood(View view) {
         Intent intent = new Intent (this, NutritionActivity.class);
         startActivity(intent);
+    }
+
+
+    // Log custom foods (calories and CO2)
+    // TODO: Add Functionality for CO2 tracking, it's currently disabled!
+    public void logCustom(View view) {
+        final int cals, co2;
+        cals = Integer.parseInt(customCals.getText().toString());
+        //co2 = Integer.parseInt(customCO2.getText().toString());
+
+        mDatabase.child("Users").child(currentUser.getUid()).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if (!task.isSuccessful()) {
+                    Log.e("firebase", "Error getting data", task.getException());
+                }
+                else {
+                    Log.d("firebase", String.valueOf(task.getResult().getValue()));
+                    int currCals = Integer.parseInt(task.getResult().child("currentCalories").getValue().toString());
+                    int newCals = cals + currCals;
+
+                    // Update current calories display and current calories in the databse
+                    mDatabase.child("Users").child(currentUser.getUid()).child("currentCalories").setValue(newCals);
+                    String currCalsString = String.valueOf(newCals);
+                    currentCaloriesText.setText(currCalsString + " of");
+                }
+            }
+        });
+    }
+
+    public void resetCals(View view) {
+        mDatabase.child("Users").child(currentUser.getUid()).child("currentCalories").setValue(0);
+        currentCaloriesText.setText("0 of");
     }
 }
