630fcd56516bb85cf6f470d3032cdfb3af84b219
tydolla00 <90355178+tydolla00@users.noreply.github.com>
2021-12-07 14:34:28 -0500

    Adding functionality to the workout page

---
+++ PaceEats/app/src/main/java/com/example/paceeats/WorkoutActitivity.java
@@ -0,0 +1,34 @@
+package com.example.paceeats;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class WorkoutActitivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_workout_actitivity);
+    }
+    public void launchHome(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+    public void launchNutrition(View view) {
+        Intent intent = new Intent(this, NutritionActivity.class);
+        startActivity(intent);
+    }
+
+    public void launchWeight(View view) {
+        Intent intent = new Intent(this, WeightActivity.class);
+        startActivity(intent);
+    }
+
+    public void launchMore(View view) {
+        Intent intent = new Intent(this, MoreActivity.class);
+        startActivity(intent);
+    }
+}
