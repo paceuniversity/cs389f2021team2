3c78805e722567b1c5f1eb4416f5a9d243c0f996
Kevin Lutz <kevinlutz429@gmail.com>
2021-12-28 06:44:28 -0500

    set up project for play store

---
+++ PaceEats/app/src/main/java/me/kevinlutz/paceeats/MoreActivity.java
@@ -0,0 +1,58 @@
+package me.kevinlutz.paceeats;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_more);
+    }
+
+
+    public void launchHome(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void launchNutrition(View view) {
+        Intent intent = new Intent(this, NutritionActivity.class);
+        startActivity(intent);
+    }
+
+    public void launchWeight(View view) {
+        Intent intent = new Intent(this, WeightActivity.class);
+        startActivity(intent);
+    }
+
+    public void launchMore(View view) {
+        Intent intent = new Intent(this, MoreActivity.class);
+        startActivity(intent);
+    }
+
+    public void LaunchFoodRecommendations(View view) {
+        Intent intent = new Intent(this, FoodRecommendations.class);
+        startActivity(intent);
+    }
+
+
+    public void launchSettings(View view) {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
+    }
+
+    public void launchWorkout(View view){
+        Intent intent = new Intent(this,WorkoutActitivity.class);
+        startActivity(intent);
+    }
+
+    public void launchAbout(View view) {
+        startActivity(new Intent(this, AboutActivity.class));
+    }
+
+}
